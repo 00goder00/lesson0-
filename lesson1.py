3e6782b71dc794703f6a1c52eb9f0883a49d7bc2
00goder00 <116880866+00goder00@users.noreply.github.com>
2024-12-03 21:47:32 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,21 @@
+print(5)
+print(type(5)) #int - integer (целое число)
+print(5 + 5)
+print(5 - 5)
+print(5 * 5)
+print(5 / 5) # // - целочисленное деление, / - проосто деление
+print(5 % 5) # % нахождение остатка деления
+print(5 ** 2) #возведение в степень
+
+print(2.0)
+print(type(2.0)) # тип float
+
+print("Hello, world")
+print(type("hello world")) # string - строка
+print('Hello, ' + 'world')
+print(True, False) # boolen тип данных
+print(type(True),type(False))
+print(5 > 10, 10 > 5)
+print(1, 2, 3, 4, 5, 'hello world', True)
+print(5 <= 5, 5 >= 5, 5 == 5, 5 != 5) # != - не ровно
+print(5 == 5 and 5 < 10) # строгий оператор and
